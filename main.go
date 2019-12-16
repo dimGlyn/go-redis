0f61d308e8560643e717517e3588267536cabed7
dg <dim.glynatsis@gmail.com>
2019-12-17 00:35:34 +0200

    add proccessChunk

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"strings"
 
 	"github.com/go-redis/redis/v7"
 )
 
-var client *redis.Client
+var clientWallaces *redis.Client
+var clientEndeavors *redis.Client
+var clientAngler *redis.Client
 
 type shortURL struct {
 	Hits         string `json:"hits"`
@@ -23,37 +25,38 @@ type shortURL struct {
 }
 
 type shortUrlsData struct {
-	Type         string   `json:"type"`
-	ShortURLData shortURL `json:"value"`
+	Type string   `json:"type"`
+	data shortURL `json:"value"`
 }
 
-type JSONData []map[string]shortUrlsData
+type jsonData map[string]shortUrlsData
 
 type campaign struct {
 	campaignID, marketID string
 }
 
 var campaigns []campaign
+var data []jsonData
 
 func init() {
-	client = redis.NewClient(&redis.Options{
+	clientWallaces = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       1,
 	})
 
-	err := parseCampaignCSV("campaignMarketAssign.csv")
-
-	parseJSONshortURLs("test.json")
-
-	if err != nil {
-		log.Fatalln(err)
-	}
+	parseCampaignCSV("campaignMarketAssign.csv")
+	parseJSONshortURLs("dump.json")
 }
 
 func main() {
-	pong, err := client.Ping().Result()
+	pong, err := clientWallaces.Ping().Result()
 	fmt.Println(pong, err)
+
+	for i, chunk := range data {
+		fmt.Printf("proccess chunk %d\n", i)
+		proccessChunk(chunk)
+	}
 }
 
 func parseCampaignCSV(filepath string) error {
@@ -81,13 +84,7 @@ func parseCampaignCSV(filepath string) error {
 func parseJSONshortURLs(filepath string) error {
 	file, _ := ioutil.ReadFile(filepath)
 
-	fmt.Println(file)
-
-	var shortUrls JSONData
-
-	_ = json.Unmarshal([]byte(file), &shortUrls)
-
-	fmt.Println(shortUrls[0]["joi:shortURLCodes:jEOeTEd9"].ShortURLData)
+	_ = json.Unmarshal([]byte(file), &data)
 
 	return nil
 }
@@ -99,4 +96,28 @@ func getMarket(campaignID string) (string, error) {
 		}
 	}
 	return "nil", errors.New("Invalid campaignID")
+}
+
+func proccessChunk(chunk jsonData) {
+	fmt.Println(chunk["joi:shortURLCodes:dq79oUVt"])
+	shorturl := chunk["joi:shortURLCodes:dq79oUVt"]
+	targeturl := shorturl.data.TargetURL
+
+	if strings.Contains(targeturl, "/ca/0") {
+		handleOptinUrl(shorturl)
+	} else {
+		handleCampaignUrl(shorturl)
+	}
+
+	// for k, value := range chunk {
+	// 	fmt.Println("joi:shortURLCodes:dq79oUVt")
+	// }
+}
+
+func handleCampaignUrl(shorturl shortUrlsData) {
+
+}
+
+func handleOptinUrl(shorturl shortUrlsData) {
+
 }
